246975cc032b95311766f1b0c658035383444e99
Clipperai <166284550+Clipperai@users.noreply.github.com>
2026-03-22 20:12:49 +0530

    Add example for sending WhatsApp messages

--- examples/example_message.py
+++ examples/example_message.py
@@ -1,1 +1,10 @@
+from src.main import send_whatsapp_message
 
+phone = "+91XXXXXXXXXX"
+message = "Hello from WhatsApp Automation Project"
+hour = 18
+minute = 30
+
+send_whatsapp_message(phone, message, hour, minute)
+
+print("Example executed successfully")
