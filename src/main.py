16ab84c8846c5e66de6bfae38397501d892f2721
Clipperai <166284550+Clipperai@users.noreply.github.com>
2026-03-22 19:29:22 +0530

    Add message scheduling feature using pywhatkit

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
+import pywhatkit as kit
 
+phone_num = input("Enter receiver's mobile number (with country code): ")
+
+message = "Hello, this message was sent using python automation."
+
+hour = 19
+minute = 21
+
+kit.sendwhatmsg(phone_num, message, hour, minute)
+
+print("Message Scheduled")
